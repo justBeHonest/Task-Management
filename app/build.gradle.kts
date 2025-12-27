aea67c81cb56adb7784dac6aa474e47b902761be
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2025-12-27 08:31:35 +0300

    coil library added for pick image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
     val navVersion = "2.9.6"
 
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
